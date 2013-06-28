f774d38b74242dfe5d5496474d843184a6f38c68
wadesweatt <wade.sweatt@gmail.com>
2013-06-28 17:25:43 -0400

    Initial Commit

---
+++ CalendarStyleDatePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalendarStyleDatePicker
+//
+//  Created by Wade Sweatt on 6/28/13.
+//  Copyright (c) 2013 J. Wade Sweatt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CalendarStyleDatePicker/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  CalendarStyleDatePicker
+//
+//  Created by Wade Sweatt on 6/28/13.
+//  Copyright (c) 2013 J. Wade Sweatt. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
