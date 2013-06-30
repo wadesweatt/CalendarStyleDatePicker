ad0f37320c0dc15de226565f59a38a59b7ed722f
Wade Sweatt <wade.sweatt@gmail.com>
2013-06-29 21:30:10 -0400

    Add copyright statement.

--- CalendarStyleDatePicker/RVDatePicker.h
+++ CalendarStyleDatePicker/RVDatePicker.h
@@ -4,6 +4,12 @@
 //
 //  Created by Wade Sweatt on 5/30/13.
 //
+//  Permission is given to use this source code file without charge in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
 
 #import <Cocoa/Cocoa.h>
 
